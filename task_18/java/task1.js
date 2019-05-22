fa85d7b10691cd12957f12fac7665329fd723178
yogashreeck <ykumarostb1@gmail.com>
2019-05-22 06:25:57 +0530

    second

--- task_18/java/task1.js
+++ task_18/java/task1.js
@@ -1,5 +1,12 @@
-var first, second,result;  
- first= 5;
- second = 2;
- result="first"+"second"
-document.write(first+"+"second"="+result);
+var x= 5;
+    y = 2;
+    add=x+y;
+    sub=x-y;
+    mul=x*y;
+    div=x/y;
+document.write("first="+x+' ');
+document.write("second="+y+'<br>');
+document.write("Add"+' '+x+"+"+y+"="+add+"<br>");
+document.write("sub"+' '+x+"-"+y+"="+sub+"<br>");
+document.write("mul"+' '+x+"*"+y+"="+mul+"<br>");
+document.write("div"+' '+x+"/"+y+"="+div+"<br>");
