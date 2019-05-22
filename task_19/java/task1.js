f0fa4edac808df269eab9c043660c162b35197e2
yogashreeck <you@example.com>
2019-05-22 16:27:20 +0530

    one

---
+++ task_19/java/task1.js
@@ -0,0 +1,41 @@
+debugger;
+var a= 12;
+var b= 2016;
+
+ switch(condtion){
+        case 1: a = "31 days";
+            break;
+        case 3: a = "31 days";
+            break;
+        case 5: a = "31 days";
+            break;
+        case 7: a = "31 days";
+        break;
+        case 9: a= "31 days" ;
+            break;
+        case 12: a= "31 days" ;
+            break;
+            document.write("month is leap"+a)
+          }
+          switch(condtion2){
+        case 2: b = "28 days";
+            break;
+        case 6: b = "June"; 
+            break;
+        case 7: b = "July";
+            break;
+        case 8: b = "August";
+            break
+        case 9: b = "September";
+            break;
+        case 10: b = "October";
+            break;
+        case 11: b = "November";
+            break;
+        case 12: b = "December";
+            break;
+        }
+if(b==2){
+
+    document.write();
+}
