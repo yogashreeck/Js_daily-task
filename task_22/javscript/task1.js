ffaaed0eac2f6c13db6901b2b5bade5cfe7d3ce9
yogashreeck <you@example.com>
2019-05-28 16:35:50 +0530

    added

---
+++ task_22/javscript/task1.js
@@ -0,0 +1,16 @@
+
+function fn1() {
+	var box1= document.getElementById('box1');
+	var box2= document.getElementById('box2');
+    
+
+    for (var i = 0; i < checkbox.length; i++)
+   {
+       if (checkbox[i].checked)
+       {
+           alert("Selected = " + label[i].innerHTML);
+       }
+  }
+ 
+
+ 
