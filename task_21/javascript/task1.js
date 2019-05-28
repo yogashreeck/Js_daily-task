4b4ad378c05b0d7231c788a1f341cec5afc8479b
yogashreeck <you@example.com>
2019-05-28 16:32:50 +0530

    first

--- task_21/javascript/task1.js
+++ task_21/javascript/task1.js
@@ -1,8 +1,8 @@
-function Country(country_name)
-  {
-  return country_name.reduce(function(lname, country) 
-  {
-    return lname.length > country.length ? lname : country;
-  }, 
-"");
+function longest_string(str_ara) {
+  var max = str_ara[0].length;
+  str_ara.map(v => max = Math.max(max, v.length));
+  result = str_ara.filter(v => v.length == max);
+  return result;
 }
+
+document.write(longest_string(['india', 'america', 'united kingdom']));
--- task_21/javascript/task3.js
+++ task_21/javascript/task3.js
@@ -1,4 +1,33 @@
-var pdbl = $( "p:first" );
-pdbl.dblclick(function() {
-pdbl.toggleClass( "dbl" );
-});
+function fn1(){
+	if (document.getElementById('box1').style.backgroundColor=="red") {
+		document.getElementById('box1').style.backgroundColor="white";
+}
+	else{
+		document.getElementById('box1').style.backgroundColor="red";
+	}
+
+}
+
+function fn2(){
+	if (document.getElementById('box2').style.backgroundColor=="blue") {
+		document.getElementById('box2').style.backgroundColor="white";
+
+  
+	}
+	else{
+		document.getElementById('box2').style.backgroundColor="blue";
+	}
+
+}
+
+function fn3(){
+	if (document.getElementById('box3').style.backgroundColor=="green") {
+		document.getElementById('box3').style.backgroundColor="white";
+
+  
+	}
+	else{
+		document.getElementById('box3').style.backgroundColor="green";
+	}
+
+}
