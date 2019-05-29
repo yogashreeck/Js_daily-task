293fb69764ad1fa432b1fbfc6ce53cc594d3160c
yogashreeck <you@example.com>
2019-05-29 10:22:54 +0530

    last

--- task_22/javscript/task1.js
+++ task_22/javscript/task1.js
@@ -1,16 +1,30 @@
 
 function fn1() {
-	var box1= document.getElementById('box1');
-	var box2= document.getElementById('box2');
-    
+	var items=document.getElementsByName('nam');
+	for(var i=0; i<items.length; i++){
+		if (items[i].type=='checkbox')
+			items[i].checked=true;
+	}
+}
+function fn2() {
+	var items=document.getElementsByName('nam');
+	for(var i=0; i<items.length; i++){
+		if (items[i].type=='checkbox')
+			items[i].checked=false;
+	}
+}
+function fn3() {
+	var select=0;
+	var items=document.getElementsByName('nam');
+	for(var i=0; i<items.length; i++){
+		if (items[i].type=='checkbox' && items[i].checked==true)
+			select=parseInt(items[i].value)+select;
+	}
+	
+	document.getElementById('total').innerHTML="total is="+select;
+	// document.write("total is="+select);
 
-    for (var i = 0; i < checkbox.length; i++)
-   {
-       if (checkbox[i].checked)
-       {
-           alert("Selected = " + label[i].innerHTML);
-       }
-  }
- 
+}
 
+   
  
